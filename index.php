2219ad01531698b1dc8b51a0b0305f35ad3d5d3c
Backerss <AsanRodnuan2546@gmail.com>
2023-09-06 20:21:32 +0700

    เปลี่ยนหน้า index

--- index.php
+++ index.php
@@ -1,111 +1,44 @@
-
-<?php
-
-    session_start();
-
-    if(!isset($_SESSION["user"])){
-        header("Location: page/auth/login");
-    }
-
-
-
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Thai Airways Landing Page</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
-  <link rel="icon" href="img/tha_icon.ico" type="image/x-icon">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/custom.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <title>BUNNY VIR</title>
 </head>
 <body>
+        </nav>
+        <main class="header">
+            <div class="test col-lg-6 mx-auto p-3 text-center">
+                <h1 class="text-center">WELCOME TO BUNNY VIRTUAL</h1>
+                <p class="text-center">For you Apply to Bunny</p>
+                <a href="page/auth/login" class="btn btn-light">CREW LOGIN</a>
+            </div>
+        </main>
 
-    <?php include 'page/inc/navbar.php' ?>
-
-    <div class="jumbotron text-center p-5">
-        <h1 class="display-4">Welcome to Thai Airways</h1>
-        <p class="lead">Experience the elegance of flying with us.</p>
-        <a class="btn btn-purple btn-lg" href="#" role="button">Flight</a>
-    </div>
-
-    <div class="container mt-5 p-lg-0">
-        <div class="row">
-            <div class="col-lg-6 border p-2 shadow-1">
-                <h1 class="text-center">LIVE MAP!</h1>
-                <div class="ratio ratio-21x9">
-                    <iframe src="https://tfdidesign.com/core/flightmap.php?theme=dark&noscrollwheel=true&region=5" allowfullscreen></iframe>
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="text-center p-2">BUNNY VIR</h1>
                 </div>
             </div>
-            <div class="col-lg-6">
-                <h2 class="text-center" >Flight Book!</h2>
-                <div class="d-grid gap-2">
-                    <button class="btn btn-purple" type="button">BOOK FLIGHT</button>
-                    <button class="btn btn-primary" type="button">CRRUENT FLIGHT</button>
+            <div class="row">
+                <div class="col-lg-6 text-center">
+                   <img style="border-radius: 120vh; width: 290px; height: 290px;" class="img-fluid" src="https://theredsvirtual.com/images/d6b8428d-c29a-4b5e-92cf-a8c7ad7a5f1a.png" alt="">
+                </div>
+                <div class="col-lg-6">
+                    <h1>Welcome to BUUNY</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nobis tempore laboriosam eos nisi rem minus ducimus totam. Eos fugit incidunt architecto ipsa autem totam pariatur optio provident numquam? Tempora!</p>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-12 border mt-5 shadow-1 p-2">
-                <h5>Current Flights</h5>
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">Pilot</th>
-                        <th scope="col">Callsign</th>
-                        <th scope="col">Arrival</th>
-                        <th scope="col">Aircraft</th>
-                        <th scope="col">ETE/ETD</th>
-                        <th scope="col">Distance</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Network</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>THA1</td>
-                        <td>VTCC</td>
-                        <td>A320</td>
-                        <td>1.10</td>
-                        <td>400</td>
-                        <td class="bg-primary">Enroute</td>
-                        <td>IVAO</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>THA2</td>
-                        <td>VTCC</td>
-                        <td>A320</td>
-                        <td>1.10</td>
-                        <td>400</td>
-                        <td class="bg-success">Enroute</td>
-                        <td>IVAO</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td>THA3</td>
-                        <td>VTCC</td>
-                        <td>A320</td>
-                        <td>1.10</td>
-                        <td>400</td>
-                        <td>Enroute</td>
-                        <td>IVAO</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 </html>
