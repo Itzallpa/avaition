c2046199ea670574e63c2686d542439698df12ff
Backerss <AsanRodnuan2546@gmail.com>
2023-09-07 23:16:37 +0700

    เปลี่ยน sql

--- sql/startup_airliner.sql
+++ sql/startup_airliner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2023 at 09:53 AM
+-- Generation Time: Sep 07, 2023 at 06:16 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,13 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `route`
+-- Table structure for table `airport`
 --
 
-CREATE TABLE `route` (
+CREATE TABLE `airport` (
   `id` int(11) NOT NULL,
-  `airport_name` varchar(60) NOT NULL
+  `airport_name` varchar(60) NOT NULL,
+  `icao` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `airport`
+--
+
+INSERT INTO `airport` (`id`, `airport_name`, `icao`) VALUES
+(1, 'Bangkok BKK', 'VTBS'),
+(2, 'Phnom Penh PNH', 'VDPP'),
+(3, 'Delhi DEL', 'VIDP'),
+(4, 'Tokyo NRT', 'RJAA'),
+(5, 'Singapore SIN', 'WSSS'),
+(6, 'Jakarta CGK', 'WIII'),
+(7, 'Hong Kong HKG', 'VHHH'),
+(8, 'Seoul ICN', 'RKSI'),
+(9, 'Melbourne MEL', 'YMML'),
+(10, 'Taipei TPE', 'RCTP'),
+(11, 'Osaka KIX', 'RJBB'),
+(12, 'Sydney SYD', 'YSSY'),
+(13, 'Kuala Lumpur KUL', 'WMKK'),
+(14, 'Denpasar DPS', 'WADD'),
+(15, 'Yangon RGN', 'VYYY'),
+(16, 'Beijing PEK', 'ZBAA'),
+(17, 'Dhaka DAC', 'VGHS'),
+(18, 'Guangzhou CAN', 'ZGGG'),
+(19, 'Shanghai PVG', 'ZSPD'),
+(20, 'Chengdu TFU', 'ZUTF'),
+(21, 'Vientiane VTE', 'VLVT'),
+(22, 'Frankfurt FRA', 'EDDF'),
+(23, 'London LHR', 'EGLL'),
+(24, 'Tokyo HND', 'RJTT'),
+(25, 'Manila MNL', 'RPLL'),
+(26, 'Zurich ZRH', 'LSZH'),
+(27, 'Copenhagen CPH', 'EKCH'),
+(28, 'Chennai MAA', 'VOMM'),
+(29, 'Lahore LHE', 'OPLA'),
+(30, 'Ahmedabad AMD', 'VAAH'),
+(31, 'Hyderabad HYD', 'VOHS');
 
 -- --------------------------------------------------------
 
@@ -46,7 +84,7 @@ CREATE TABLE `users` (
   `user_vatsim_id` varchar(20) NOT NULL DEFAULT 'None',
   `birthdate` varchar(60) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `profile_picture` varchar(255) NOT NULL
+  `profile_picture` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -54,9 +92,9 @@ CREATE TABLE `users` (
 --
 
 --
--- Indexes for table `route`
+-- Indexes for table `airport`
 --
-ALTER TABLE `route`
+ALTER TABLE `airport`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -70,10 +108,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `route`
+-- AUTO_INCREMENT for table `airport`
 --
-ALTER TABLE `route`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `airport`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `users`
