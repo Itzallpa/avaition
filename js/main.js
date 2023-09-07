1022c40df1ddb0dd793db4a4ed13b1a552982ffb
Backerss <AsanRodnuan2546@gmail.com>
2023-09-07 23:12:18 +0700

    กำลังแก้ไข main.js ส่วนของ map

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
-
-
-
-//when on page load
-$(document).ready(function() {
-
+$(document).ready(function () {
     var map = L.map('map').setView([51.505, -0.09], 13); // ตำแหน่งและการซูมเริ่มต้น
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var marker;
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-
-    $('#sel_airport').change(function() {
-
-
-        //setViem on map
-        var map = L.map('map').setView([51.505, -0.09], 13); // ตำแหน่งและการซูมเริ่มต้น
-            var airport = $(this).val();
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/search?query=vtbs">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        
-        
-
+    $('#sel_airport').change(function () {
+        var selectedAirport = $(this).val();
+        // ดำเนินการตรวจสอบค่า selectedAirport และกำหนดตำแหน่งใหม่ของแผนที่
+        if (selectedAirport === 'YOUR_AIRPORT_ID_1') {
+            map.setView([LATITUDE_1, LONGITUDE_1], ZOOM_LEVEL);
+        } else if (selectedAirport === 'YOUR_AIRPORT_ID_2') {
+            map.setView([LATITUDE_2, LONGITUDE_2], ZOOM_LEVEL);
+        }
+        // ลบมาร์คเก่า (ถ้ามี) และสร้างมาร์คใหม่
+        if (marker) {
+            marker.remove();
+        }
+        marker = L.marker([NEW_LATITUDE, NEW_LONGITUDE]).addTo(map);
     });
-
 });
-
