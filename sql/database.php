99ed877934935619634cbe34e801bf261096050e
Backerss <AsanRodnuan2546@gmail.com>
2023-09-12 03:33:59 +0700

    database

--- sql/database.php
+++ sql/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "startupairliner";
+$hostName = "thepurple.online";
+$dbUser = "p2048873";
+$dbPassword = "ann9cyczdkj@jJ";
+$dbName = "p2048873_avaition";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
