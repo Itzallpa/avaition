83643f5079d91b7973cae5f69639ae50b4ca27b1
Natakorn <guy.panmanee@gmail.com>
2023-09-14 23:53:48 +0700

    change name

--- index.php
+++ index.php
@@ -91,7 +91,7 @@
         <div class="row">
           <div class="col-lg-6 mx-auto text-center">
             <img src="img/tha-logo.png" alt="" width="70" height="70" srcset="">
-            <p style="font-size: 13px">Thai airways virtual airline is a non-profit organization. This website serve the hobby of flight simulation. Similarities with real existing airlines are not intended and The Reds Virtual is in no way related to, funded by, or recognized by the real airlines.</p>
+            <p style="font-size: 13px">Thai airways virtual airline is a non-profit organization. This website serve the hobby of flight simulation. Similarities with real existing airlines are not intended and Thai Airway VA is in no way related to, funded by, or recognized by the real airlines.</p>
             <br>
             <p>Copyright © 2023 Thai airways virtual airlinel</p>
           </div>
