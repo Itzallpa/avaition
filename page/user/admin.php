2394bc65411f1c0c0ba72fe69c359b5f04408a75
Shunji Maeda <santachannel123@gmail.com>
2023-09-23 18:38:11 +0700

    -SF Pro font added (Apple font for whole pages). -Awesome font/Icon added for whole pages.

--- page/user/admin.php
+++ page/user/admin.php
@@ -33,14 +33,16 @@
     <title>HOME PAGE | Thai Airway VA</title>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-
+    <!-- Awesome ICON -->
+    <link href="https://kit-pro.fontawesome.com/releases/v6.4.2/css/pro.min.css" rel="stylesheet">
     <!-- Simple bar CSS -->
     <link rel="stylesheet" href="../../assets/css/simplebar.css">
     <!-- Fonts CSS -->
     <link
         href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
+    <!-- Font SF PRO DISPLAY (APPLE) -->
+    <link href="https://fonts.cdnfonts.com/css/sf-pro-display?styles=98774,98773,98775,98770,98771,98769" rel="stylesheet">
     <!-- Icons CSS -->
     <link rel="stylesheet" href="../../assets/css/feather.css">
     <link rel="stylesheet" href="../../assets/css/dataTables.bootstrap4.css">
