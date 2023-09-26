38a45a17833c02a9d28b311ed777958b016b5bab
Backerss <AsanRodnuan2546@gmail.com>
2023-09-27 01:50:24 +0700

    เพิ่มระบบ post

--- js/main.js
+++ js/main.js
@@ -1145,17 +1145,42 @@ $(document).ready(function () {
 
         var docs = JSON.parse(data);
         $('#show-editor').html(docs.pilot_train_docs);
+
+        editors.setData(docs.pilot_train_docs); 
         
     });
 
    
     $('#submit-editer').click(function () {
 
-        var data = editors.getData();
+        var data_post = editors.getData();
 
-        console.log(data);
-        $('#show-editor').html(data);
-    
+        $.ajax({
+            url: "../auth/server_post.php",
+            method: "POST",
+            data: {
+                type: "edit_docs_pilot",
+                data: data_post
+            },
+            success: function (data) {
+                if (data) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Edit Docs Success',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                    $('#show-editor').html(data_post);
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong! Edit Docs Failed!',
+                    })
+                }
+            }
+        })
+
 
     
     });
