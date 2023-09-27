57b3c34bdb4cb9847f4048597a1f56a45223f315
Backerss <AsanRodnuan2546@gmail.com>
2023-09-28 01:57:24 +0700

    อัพเดทฐาน ข้อมูล .sql

--- sql/startup_airliner.sql
+++ sql/startup_airliner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 16, 2023 at 09:11 PM
+-- Generation Time: Sep 27, 2023 at 08:55 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `aircraft` (
   `aircraft_id` int(11) NOT NULL,
-  `aircraft_name` varchar(60) NOT NULL,
-  `aircraft_type` varchar(30) NOT NULL,
-  `aircraft_reg` varchar(30) NOT NULL,
-  `aircraft_add_date` datetime NOT NULL,
+  `aircraft_name` varchar(60) NOT NULL DEFAULT 'Unknow',
+  `aircraft_type` varchar(30) NOT NULL DEFAULT 'None',
+  `aircraft_reg` varchar(30) NOT NULL DEFAULT 'XX-XXX',
+  `aircraft_add_date` varchar(30) NOT NULL DEFAULT current_timestamp(),
   `aircraft_add_by` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -68,19 +68,79 @@ CREATE TABLE `flights` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `rank_pilot`
+--
+
+CREATE TABLE `rank_pilot` (
+  `rank_id` int(11) NOT NULL,
+  `rank_name` varchar(50) NOT NULL DEFAULT 'Unknow',
+  `rank_hour` int(11) NOT NULL DEFAULT 5,
+  `rank_note` text NOT NULL DEFAULT '-not request-'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rank_pilot`
+--
+
+INSERT INTO `rank_pilot` (`rank_id`, `rank_name`, `rank_hour`, `rank_note`) VALUES
+(1, 'Student Pilot', 0, '-not request-'),
+(2, 'First Officer', 10, '-not request-'),
+(3, 'Senior First Officer', 25, '-not request-'),
+(4, 'Captain', 50, '-not request-'),
+(5, 'Senior Captain', 100, '-not request-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record`
+--
+
+CREATE TABLE `record` (
+  `log_id` int(11) NOT NULL,
+  `log_user` varchar(60) NOT NULL DEFAULT 'Unknow',
+  `log_role` varchar(20) NOT NULL,
+  `log_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `log_details` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `server_post`
+--
+
+CREATE TABLE `server_post` (
+  `id` int(11) NOT NULL,
+  `pilot_train_docs` text NOT NULL DEFAULT 'None'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `server_post`
+--
+
+INSERT INTO `server_post` (`id`, `pilot_train_docs`) VALUES
+(1, '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `full_name` varchar(128) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `full_name` varchar(128) NOT NULL DEFAULT 'None',
+  `email` varchar(255) NOT NULL DEFAULT 'None',
   `user_role` varchar(60) NOT NULL DEFAULT 'Member',
+  `rank` int(11) NOT NULL DEFAULT 1,
+  `flight_hour` int(11) NOT NULL DEFAULT 0,
+  `flight_flow` int(11) NOT NULL DEFAULT 0,
   `user_ivao_id` varchar(20) NOT NULL DEFAULT 'None',
   `user_vatsim_id` varchar(20) NOT NULL DEFAULT 'None',
-  `birthdate` varchar(60) NOT NULL,
+  `birthdate` varchar(60) NOT NULL DEFAULT current_timestamp(),
   `password` varchar(255) NOT NULL,
-  `profile_picture` longtext NOT NULL
+  `profile_picture` longtext NOT NULL DEFAULT 'None',
+  `user_comment` varchar(120) NOT NULL DEFAULT 'None'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -104,6 +164,24 @@ ALTER TABLE `airport`
 --
 ALTER TABLE `flights`
   ADD PRIMARY KEY (`flight_id`);
+
+--
+-- Indexes for table `rank_pilot`
+--
+ALTER TABLE `rank_pilot`
+  ADD PRIMARY KEY (`rank_id`);
+
+--
+-- Indexes for table `record`
+--
+ALTER TABLE `record`
+  ADD PRIMARY KEY (`log_id`);
+
+--
+-- Indexes for table `server_post`
+--
+ALTER TABLE `server_post`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -132,6 +210,24 @@ ALTER TABLE `airport`
 --
 ALTER TABLE `flights`
   MODIFY `flight_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rank_pilot`
+--
+ALTER TABLE `rank_pilot`
+  MODIFY `rank_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `record`
+--
+ALTER TABLE `record`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `server_post`
+--
+ALTER TABLE `server_post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
